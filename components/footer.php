e9f5f000eefbba1bc197192bda9cf4edffae0d36
Mrigaank-9 <130607343+Mrigaank-9@users.noreply.github.com>
2024-06-15 14:57:27 +0530

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -1,62 +1,60 @@
-
-    <!-- Footer -->
-    <footer class="bg-dark text-white text-center text-lg-start mt-5">
-        <div class="container p-4">
-            <div class="row">
-                <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">About Cohort Hive</h5>
-                    <p>
-                       Cohort Hive is where developers unite and innovate together. Join us to create, share, and collaborate on exciting projects.
-                    </p>
-                </div>
-                <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">Quick Links</h5>
-                    <ul class="list-unstyled mb-0">
-                        <li>
-                            <a href="#" class="text-white text-decoration-none">Home</a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-white text-decoration-none">Features</a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-white text-decoration-none">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">Follow Us</h5>
-                    <ul class="list-unstyled d-flex justify-content-center">
-                        <li>
-                            <a href="https://www.linkedin.com/in/rajeevsingh2412/" class="text-white text-decoration-none me-3"><i class="fab fa-facebook-f">Rajeev Singh</i></a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-white text-decoration-none me-3"><i class="fab fa-twitter"></i></a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-white text-decoration-none me-3"><i class="fab fa-linkedin-in"></i></a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-white text-decoration-none"><i class="fab fa-github"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">Contact Us</h5>
-                    <ul class="list-unstyled mb-0">
-                        <li>
-                            <i class="fas fa-envelope me-2"></i> info@collabhub.com
-                        </li>
-                        <li>
-                            <i class="fas fa-phone me-2"></i> +1 234 567 890
-                        </li>
-                        <li>
-                            <i class="fas fa-map-marker-alt me-2"></i> Chandigarh University
-                        </li>
-                    </ul>
-                </div>
+<footer class="bg-dark text-white text-center text-lg-start mt-5">
+    <div class="container p-5">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">About Collab Hub</h5>
+                <p>
+                    Collab Hub is where developers unite and innovate together. Join us to create, share, and collaborate on exciting projects.
+                </p>
+            </div>
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Quick Links</h5>
+                <ul class="list-unstyled mb-0">
+                    <li>
+                        <a href="#" class="text-white text-decoration-none">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white text-decoration-none">Features</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white text-decoration-none">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Follow Us</h5>
+                <ul class="list-unstyled d-flex justify-content-center">
+                    <li>
+                        <a href="#" class="text-white text-decoration-none me-3"><i class="fab fa-facebook-f"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white text-decoration-none me-3"><i class="fab fa-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white text-decoration-none me-3"><i class="fab fa-linkedin-in"></i></a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white text-decoration-none"><i class="fab fa-github"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Contact Us</h5>
+                <ul class="list-unstyled mb-0">
+                    <li>
+                        <i class="fas fa-envelope me-2"></i> info@collabhub.com
+                    </li>
+                    <li>
+                        <i class="fas fa-phone me-2"></i> +1 234 567 890
+                    </li>
+                    <li>
+                        <i class="fas fa-map-marker-alt me-2"></i> 123 Developer Lane, Tech City, USA
+                    </li>
+                </ul>
             </div>
         </div>
-        <div class="text-center p-3 bg-secondary">
-            © 2024 Collab Hub
-        </div>
-    </footer>
+    </div>
+    <div class="text-center p-3 bg-secondary">
+        © 2024 Collab Hub
+    </div>
+</footer>
